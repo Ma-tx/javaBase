b52b18d0904dc9bd0b7b728f165a5f42350b3240
mtx <wsad@gmail.com>
2022-03-23 15:41:56 +0800

    pushTest

--- src/main/java/CollectionMap/CollectionDemo.java
+++ src/main/java/CollectionMap/CollectionDemo.java
@@ -16,7 +16,7 @@ public class CollectionDemo {
         list.add("hotfix");
         list.add("masterTest");
         list.add("hotfixTest");
-        list.add("pushTest");
+        list.add("pushDemo");
 
 
     }
